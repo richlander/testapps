15f9bbffe04999bfb57fabf40c7b47f23af67ad5
Richard Lander <rlander@microsoft.com>
2019-06-25 11:00:21 -0700

    Add environment info to versioninfo app

--- versioninfo/Program.cs
+++ versioninfo/Program.cs
@@ -10,13 +10,19 @@ namespace versioninfo
     {
         static void Main(string[] args)
         {
-            WriteLine(".NET Core version:");
+            WriteLine("**.NET Core info**");
             WriteLine($"Environment.Version: {Environment.Version}");
             WriteLine($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
             WriteLine($"CoreCLR Build: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
             WriteLine($"CoreCLR Hash: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
             WriteLine($"CoreFX Build: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
             WriteLine($"CoreFX Hash: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
+            WriteLine();
+            WriteLine("**Environment info**");
+            WriteLine($"Environment.OSVersion: {Environment.OSVersion}");
+            WriteLine($"RuntimeInformation.OSDescription: {RuntimeInformation.OSDescription}");
+            WriteLine($"RuntimeInformation.OSArchitecture: {RuntimeInformation.OSArchitecture}");
+            WriteLine($"Environment.ProcessorCount: {Environment.ProcessorCount}");
             WriteLine();
         }
     }
