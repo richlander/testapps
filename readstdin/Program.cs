e474949742bdcae68e644cae409a13ccb9aa981c
Richard Lander <rlander@microsoft.com>
2019-07-16 14:47:20 -0700

    Add sample for reading standard in

---
+++ readstdin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace readstdin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If using VS Code, ensure you do not use (for this app):
+            // "console": "internalConsole"
+            // Instead use:
+            // "console": "externalTerminal"
+
+            // To run this program, try:
+            // dotnet run
+            // echo foo | dotnet run
+
+            string line = null;
+            if (Console.IsInputRedirected)
+            {
+                while ((line = Console.In.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("I wanted content from standard in and only got this useless string");
+            }
+        }
+    }
+}
