9fb506472b58ec6eac383132ee67687e227ed274
Richard Lander <rlander@microsoft.com>
2019-10-14 13:49:35 -0700

    Add nameof for printing members

--- versioninfo/Program.cs
+++ versioninfo/Program.cs
@@ -11,18 +11,18 @@ namespace versioninfo
         static void Main(string[] args)
         {
             WriteLine("**.NET Core info**");
-            WriteLine($"Environment.Version: {Environment.Version}");
-            WriteLine($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
+            WriteLine($"{nameof(Environment.Version)}: {Environment.Version}");
+            WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
             WriteLine($"CoreCLR Build: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
             WriteLine($"CoreCLR Hash: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
             WriteLine($"CoreFX Build: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
             WriteLine($"CoreFX Hash: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
             WriteLine();
             WriteLine("**Environment info**");
-            WriteLine($"Environment.OSVersion: {Environment.OSVersion}");
-            WriteLine($"RuntimeInformation.OSDescription: {RuntimeInformation.OSDescription}");
-            WriteLine($"RuntimeInformation.OSArchitecture: {RuntimeInformation.OSArchitecture}");
-            WriteLine($"Environment.ProcessorCount: {Environment.ProcessorCount}");
+            WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
+            WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
+            WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
+            WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
             WriteLine();
         }
     }
