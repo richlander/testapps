5d0741a7409b9f8d878417e33e74d1f3b05321cc
Rich Lander <rlander@microsoft.com>
2020-08-21 09:02:03 -0700

    Update app to 3.1

--- versioninfo/Program.cs
+++ versioninfo/Program.cs
@@ -13,14 +13,12 @@ namespace versioninfo
             WriteLine("**.NET Core info**");
             WriteLine($"{nameof(Environment.Version)}: {Environment.Version}");
             WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
-            WriteLine($"CoreCLR Build: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
-            WriteLine($"CoreCLR Hash: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
-            WriteLine($"CoreFX Build: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
-            WriteLine($"CoreFX Hash: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
+            WriteLine($"Libraries version: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
+            WriteLine($"Libraries hash: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0].InformationalVersion.Split('+')[1]}");
             WriteLine();
             WriteLine("**Environment info**");
-            WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
             WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
+            WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
             WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
             WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
             WriteLine();
